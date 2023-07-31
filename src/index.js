79639e87075c95115359f0096059d44a87641372
Arittra-Bag <110278170+Arittra-Bag@users.noreply.github.com>
2023-07-31 21:59:53 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AmplifyAuthenticator } from '@aws-amplify/ui-react';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+Amplify.configure(awsconfig);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <AmplifyAuthenticator>
+      <App />
+    </AmplifyAuthenticator>
+  </React.StrictMode>
+);
+
+reportWebVitals();
